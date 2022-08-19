c64eee9bb8aa430032e53661285daccd6264e4d3
Akshayw26 <wangade30@gmail.com>
2022-08-19 17:04:24 +0530

    Added - Win or Loss

--- src/com/gamblingsimulatorprogram/GamblingSimulator.java
+++ src/com/gamblingsimulatorprogram/GamblingSimulator.java
@@ -3,8 +3,16 @@ package com.gamblingsimulatorprogram;
 public class GamblingSimulator {
 
     int stake = 100;
-    final int dailyBet = 1;
+    static final int dailyBet = 1;
 
+    public static void gambler() {
 
+        int result = (int) Math.floor(Math.random() * 10 % 2);
+        if( dailyBet == result) {
+            System.out.println("Winner");
+        }else {
+            System.out.println("loser");
+        }
+    }
 
 }
--- src/com/gamblingsimulatorprogram/GamblingSimulatorMain.java
+++ src/com/gamblingsimulatorprogram/GamblingSimulatorMain.java
@@ -1,10 +1,14 @@
 package com.gamblingsimulatorprogram;
 
+import static com.gamblingsimulatorprogram.GamblingSimulator.gambler;
+
 public class GamblingSimulatorMain {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Gambling Simulator Program");
+        System.out.println("-------------------------------------");
+           gambler();
 
     }
 }
