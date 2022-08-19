7cbeb9493538a0e8fbd77adfc9fb56afa48648e5
Akshayw26 <wangade30@gmail.com>
2022-08-19 16:56:28 +0530

    Added - Start betting

---
+++ src/com/gamblingsimulatorprogram/GamblingSimulator.java
@@ -0,0 +1,10 @@
+package com.gamblingsimulatorprogram;
+
+public class GamblingSimulator {
+
+    int stake = 100;
+    final int dailyBet = 1;
+
+
+
+}
---
+++ src/com/gamblingsimulatorprogram/GamblingSimulatorMain.java
@@ -0,0 +1,10 @@
+package com.gamblingsimulatorprogram;
+
+public class GamblingSimulatorMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Gambling Simulator Program");
+
+    }
+}
